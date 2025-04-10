bb6b49c663590f0f4e71edb3f667f2f0f82e6205
kelly-u <gleicekellymacedodd@gmail.com>
2025-04-10 17:32:57 -0300

    Ajustando o mapa para localização do usuário - ainda sema dicionar novas cidades

--- app/src/main/java/br/com/ifpe/gleicekelly/alaga/ui/pages/MapaPage.kt
+++ app/src/main/java/br/com/ifpe/gleicekelly/alaga/ui/pages/MapaPage.kt
@@ -1,37 +1,64 @@
 package br.com.ifpe.gleicekelly.alaga.ui.pages
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
+import android.content.pm.PackageManager
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import br.com.ifpe.gleicekelly.alaga.R
+import androidx.core.content.ContextCompat
 import br.com.ifpe.gleicekelly.alaga.ui.view.MainViewModel
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
+import androidx.compose.runtime.* //remember
+import android.Manifest
+import com.google.maps.android.compose.*
 
 @Composable
 fun MapaPage(viewModel: MainViewModel) {
-    Column(
-        modifier = Modifier.fillMaxSize()
-        .background(colorResource(id = R.color.yellow_alaga))
-        .wrapContentSize(Alignment.Center)
-
-    ) {
-        Text(
-            text = "Aqui vai ficar o mapa!",
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp
+    val camPosState = rememberCameraPositionState ()
+    val recife = LatLng(-8.05, -34.9)
+    val caruaru = LatLng(-8.27, -35.98)
+    val joaopessoa = LatLng(-7.12, -34.84)
+    val context = LocalContext.current
+    val hasLocationPermission by remember {
+        mutableStateOf(
+            ContextCompat.checkSelfPermission(context,
+                Manifest.permission.ACCESS_FINE_LOCATION) ==
+                    PackageManager.PERMISSION_GRANTED
+        )
+    }
+    GoogleMap (modifier = Modifier.fillMaxSize(),
+        onMapClick = { viewModel.add("Nova cidade", location = it) },
+        cameraPositionState = camPosState,
+        properties = MapProperties(isMyLocationEnabled = hasLocationPermission),
+        uiSettings = MapUiSettings(myLocationButtonEnabled = true))
+    {
+        Marker(
+            state = MarkerState(position = recife),
+            title = "Recife",
+            snippet = "Marcador em Recife",
+            icon = BitmapDescriptorFactory.defaultMarker(
+                BitmapDescriptorFactory.HUE_BLUE)
+        )
+        Marker(
+            state = MarkerState(position = caruaru),
+            title = "Caruaru",
+            snippet = "Marcador em Caruaru",
+            icon = BitmapDescriptorFactory.defaultMarker(
+                BitmapDescriptorFactory.HUE_ORANGE)
+        )
+        Marker(
+            state = MarkerState(position = joaopessoa),
+            title = "João Pessoa",
+            snippet = "Marcador em João Pessoa",
+            icon = BitmapDescriptorFactory.defaultMarker(
+                BitmapDescriptorFactory.HUE_CYAN)
         )
     }
 }
