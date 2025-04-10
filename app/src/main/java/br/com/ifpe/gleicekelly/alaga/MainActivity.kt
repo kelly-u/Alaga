545bf793ee8249949cacbeae6f5f222d5a541edb
kelly-u <gleicekellymacedodd@gmail.com>
2025-04-10 10:36:00 -0300

    Criação das páginas do mapa e dos endereços favoritos

--- app/src/main/java/br/com/ifpe/gleicekelly/alaga/MainActivity.kt
+++ app/src/main/java/br/com/ifpe/gleicekelly/alaga/MainActivity.kt
@@ -1,26 +1,15 @@
 package br.com.ifpe.gleicekelly.alaga
 
 import br.com.ifpe.gleicekelly.alaga.ui.theme.AlagaTheme
-import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import br.com.ifpe.gleicekelly.alaga.ui.MapaPage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,7 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AlagaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    HomePage(
+                    MapaPage(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -38,27 +27,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun HomePage(modifier: Modifier = Modifier) {
-    val activity = LocalContext.current as? Activity
-    Column(
-        modifier = modifier.padding(16.dp).fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "Bem-vinda(o), Usuário!",
-            fontSize = 24.sp,
-        )
-        Row(modifier = modifier) {
-            Button(
-                onClick = {
-                    activity?.finish();
-                },
-            ) {
-                Text("Sair")
-            }
-        }
-    }
-}
