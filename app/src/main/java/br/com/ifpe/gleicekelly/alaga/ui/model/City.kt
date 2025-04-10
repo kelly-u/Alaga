cc909d6cc401f915e23cee7f273bd9b88f8bd8ab
kelly-u <gleicekellymacedodd@gmail.com>
2025-04-10 17:04:52 -0300

    Configurando mapa no projeto

--- app/src/main/java/br/com/ifpe/gleicekelly/alaga/ui/model/City.kt
+++ app/src/main/java/br/com/ifpe/gleicekelly/alaga/ui/model/City.kt
@@ -1,8 +1,10 @@
 package br.com.ifpe.gleicekelly.alaga.ui.model
 
+import com.google.android.gms.maps.model.LatLng
+
 //Mudar para rua
 data class City(
     val name: String,
     val weather: String? = null,
-    val location: String? = null
+    val location: LatLng? = null
 )
--- app/src/main/java/br/com/ifpe/gleicekelly/alaga/ui/view/MainViewModel.kt
+++ app/src/main/java/br/com/ifpe/gleicekelly/alaga/ui/view/MainViewModel.kt
@@ -3,6 +3,7 @@ package br.com.ifpe.gleicekelly.alaga.ui.view
 import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.ViewModel
 import br.com.ifpe.gleicekelly.alaga.ui.model.City
+import com.google.android.gms.maps.model.LatLng
 
 class MainViewModel : ViewModel() {
     private val _cities = List(20) { i ->
@@ -15,8 +16,8 @@ class MainViewModel : ViewModel() {
         _cities.remove(city)
     }
 
-    fun add(name: String) {
-        _cities.add(City(name = name))
+    fun add(name: String, location: LatLng? = null) {
+        _cities.add(City(name = name, location = location))
     }
 
 }
